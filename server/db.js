46d52d63e212929fe3f2ff4198e2e04f28591b16
dieanterwood-dotcom <dieanterwood@gmail.com>
2026-09-21 06:05:14 +0300

    Add server-side authentication foundation

---
+++ server/db.js
@@ -0,0 +1,23 @@
+import pg from 'pg';
+import { config } from './config.js';
+
+export const pool = new pg.Pool({
+  connectionString: config.databaseUrl,
+  ssl: config.isProduction ? { rejectUnauthorized: true } : undefined,
+});
+
+export async function transaction(callback) {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const value = await callback(client);
+    await client.query('COMMIT');
+    return value;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
